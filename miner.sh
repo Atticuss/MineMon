fa265ecd10d65ac6a70cfa21ce008331507957cb
Atticuss <jonn.callahan@gmail.com>
2014-02-17 10:07:01 -0500

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+sleep 10
 export DISPLAY=:0
 export GPU_MAX_ALLOC_PERCENT=100
 export GPU_USE_SYNC_OBJECTS=1
-./cgminer --config miner.conf
+/home/miner/cgminer/cgminer -c /home/miner/cgminer/miner.conf.multi
